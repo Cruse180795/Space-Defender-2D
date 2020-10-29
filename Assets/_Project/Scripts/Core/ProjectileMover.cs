172f6a4073908f6f218b1213094e0318b4e27e65
Ryan Cruse <54505658+Cruse180795@users.noreply.github.com>
2020-10-29 14:45:20 +0000

    Player Shooting & Player Projectile Set Up

---
+++ Assets/_Project/Scripts/Core/ProjectileMover.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro.EditorUtilities;
+using UnityEngine;
+
+namespace SpaceDefender.Core
+{
+    public class ProjectileMover : MonoBehaviour
+    {
+        private enum UserType
+        { 
+            Player,
+            Enemy
+        }
+
+        [Header("Projectile Config")]
+        [SerializeField] private UserType _userType;
+        [SerializeField] private float _projectileMoveSpeed = 7.5f;
+
+
+        private void Update()
+        {
+            switch (_userType) 
+            {
+                case UserType.Player:
+                    Movement(Vector3.up, _projectileMoveSpeed);
+                    break;
+
+                case UserType.Enemy:
+                    Movement(Vector3.down, _projectileMoveSpeed);
+                    break;
+            }
+
+        }
+
+        private void Movement(Vector3 direction, float speed)
+        {
+            transform.Translate(direction * speed * Time.deltaTime);
+        }
+    }
+}
+
+
---
+++ Assets/_Project/Scripts/Player/PlayerShooting.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SpaceDefender.Player
+{
+    public class PlayerShooting : MonoBehaviour
+    {
+        [Header("Player Shooting Config")]
+        [SerializeField] private GameObject _playerProjectilePrefab;
+        [SerializeField] private float _fireRate = 0.25f;
+
+        private float _nextFire = -1f;
+
+        private void Update()
+        {
+            if (Input.GetMouseButtonDown(0) && Time.time > _nextFire)
+            {
+                FireProjectile();
+            }
+
+            
+        }
+
+        private void FireProjectile()
+        {
+            _nextFire = Time.time + _fireRate;
+            Vector3 offset = new Vector3(0f, 0.8f, 0f);
+
+            Instantiate(_playerProjectilePrefab, transform.position + offset, Quaternion.identity);
+        }
+    }
+}
+
+
