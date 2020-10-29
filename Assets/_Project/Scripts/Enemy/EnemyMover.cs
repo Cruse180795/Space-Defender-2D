64b24ffd17207b695b69578f5a17b9983fade147
Ryan Cruse <54505658+Cruse180795@users.noreply.github.com>
2020-10-29 14:54:34 +0000

    Enemy Movement

---
+++ Assets/_Project/Scripts/Enemy/EnemyMover.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SpaceDefender.Enemy
+{
+    public class EnemyMover : MonoBehaviour
+    {
+        [SerializeField] private float _enemyMoveSpeed = 5f;
+
+
+
+        private void Update()
+        {
+            Movement();
+        }
+
+
+        private void Movement()
+        {
+            transform.Translate(Vector3.down * _enemyMoveSpeed * Time.deltaTime);
+
+            if(transform.position.y < -6f)
+            {
+                transform.position = new Vector3(Random.Range(-8f, 8f), 8f, 0f);
+            }
+        }
+    }
+}
+
+
+
