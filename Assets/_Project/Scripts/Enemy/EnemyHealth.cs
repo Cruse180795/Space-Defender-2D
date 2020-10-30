4da12b5213c29145ff79bcf8c2bc93e686db4e1c
Ryan Cruse <54505658+Cruse180795@users.noreply.github.com>
2020-10-30 16:17:40 +0000

    Enemy Health - Enemies Can Now Be Killed By The Player & Players Projectiles

---
+++ Assets/_Project/Scripts/Enemy/EnemyHealth.cs
@@ -0,0 +1,57 @@
+﻿using SpaceDefender.Player;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SpaceDefender.Enemy
+{
+    public class EnemyHealth : MonoBehaviour
+    {
+        [SerializeField] private int _enemyHealth;
+
+
+        private PlayerHealth _playerHealth;
+
+
+        private void Start()
+        {
+            _playerHealth = FindObjectOfType<PlayerHealth>();
+
+            if (_playerHealth == null)
+            {
+                Debug.LogError("The PlayerHealth Is NULL");
+            }
+        }
+
+        private void OnTriggerEnter2D(Collider2D collision)
+        {
+            if (collision.CompareTag("Player"))
+            {
+                if(_playerHealth != null)
+                {
+                    _playerHealth.PlayerDamage();
+                    HandleEnemyDeath();
+                }
+            }
+
+            if(collision.CompareTag("Player Projectile"))
+            {
+                Destroy(collision.gameObject);
+                HandleEnemyDeath();
+            }
+        }
+
+
+        private void HandleEnemyDeath()
+        {
+            _enemyHealth--;
+
+            if(_enemyHealth <= 0)
+            {
+                Destroy(this.gameObject);
+            }
+        }
+    }
+}
+
+
