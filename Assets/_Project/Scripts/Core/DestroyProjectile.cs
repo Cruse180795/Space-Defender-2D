d2a0e1de9ec281c22e5b6c78ad66630387ccac3c
Ryan Cruse <54505658+Cruse180795@users.noreply.github.com>
2020-10-30 15:56:46 +0000

    Player & Enemy Projectile tide Up

---
+++ Assets/_Project/Scripts/Core/DestroyProjectile.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SpaceDefender.Core
+{
+    public class DestroyProjectile : MonoBehaviour
+    {
+        private void Update()
+        {
+            DestroyObj();
+        }
+
+
+        private void DestroyObj()
+        {
+            if(transform.position.y > 6f || transform.position.y < -6f)
+            {
+                if(transform.parent != null)
+                {
+                    Destroy(transform.parent.gameObject);
+                }
+
+                Destroy(this.gameObject);
+            }
+        }
+    }
+}
+
+
