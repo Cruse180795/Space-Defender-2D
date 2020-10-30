db473ce9ae72c148dc53ca6d8684f00dc8e93417
Ryan Cruse <54505658+Cruse180795@users.noreply.github.com>
2020-10-30 16:09:39 +0000

    Player Health - Enemies Can Now Kill The Player

---
+++ Assets/_Project/Scripts/Player/PlayerHealth.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SpaceDefender.Player
+{
+    public class PlayerHealth : MonoBehaviour
+    {
+        [Header("Player Health Config")]
+        [SerializeField] private int _playerHealth = 3;
+
+
+
+        private void OnTriggerEnter2D(Collider2D collision)
+        {
+            if(collision.CompareTag("Enemy Projectile"))
+            {
+                Destroy(collision.transform.parent.gameObject);
+                _playerHealth--;
+                HandlePlayerDeath();
+                
+            }
+        }
+
+        public void PlayerDamage()
+        {
+            _playerHealth--;
+            HandlePlayerDeath();
+        }
+
+        private void HandlePlayerDeath()
+        {
+            if (_playerHealth <= 0)
+            {
+                Destroy(this.gameObject);
+            }
+        }
+    }
+}
+
