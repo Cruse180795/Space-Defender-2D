99635656086f78dc7eacbb455392649dd4d132c0
Ryan Cruse <54505658+Cruse180795@users.noreply.github.com>
2020-10-30 15:05:42 +0000

    Enemy Shooting & Enemy Projectile Set Up

---
+++ Assets/_Project/Scripts/Enemy/EnemyShooting.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.Mathematics;
+using UnityEngine;
+
+namespace SpaceDefender.Enemy
+{
+    public class EnemyShooting : MonoBehaviour
+    {
+        [SerializeField] private GameObject _enemyProjectilePrefab;
+        [SerializeField] private float _fireRate = 0.5f;
+
+        private float _nextFire = -1f;
+
+
+
+        private void Update()
+        {
+            if(Time.time > _nextFire)
+            {
+                FireProjectile();
+            }
+        }
+
+
+        private void FireProjectile()
+        {
+            _nextFire = Time.time + _fireRate;
+            Instantiate(_enemyProjectilePrefab, transform.position, quaternion.identity);
+        }
+    }
+}
+
