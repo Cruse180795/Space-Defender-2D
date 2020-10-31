6a0182c1fbf308e40432d6a4b694dc6b3b13526c
Ryan Cruse <54505658+Cruse180795@users.noreply.github.com>
2020-10-31 13:59:33 +0000

    Power Up Movement & Prefab Set Up

---
+++ Assets/_Project/Scripts/Power Ups/PowerUpMover.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SpaceDefender.PowerUps
+{
+    public class PowerUpMover : MonoBehaviour
+    {
+        [SerializeField] private float _powerUpMoveSpeed = 5f;
+
+
+        private void Update()
+        {
+            Movement();
+        }
+
+        private void Movement()
+        {
+            transform.Translate(Vector3.down * _powerUpMoveSpeed * Time.deltaTime);
+
+            if(transform.position.y < -6f)
+            {
+                Destroy(this.gameObject);
+            }
+        }
+    }
+}
+
