0a217a5d36d69f12d843715660e184b960b9c89f
Ryan Cruse <54505658+Cruse180795@users.noreply.github.com>
2020-11-01 03:06:10 +0000

    Power Ups Have A Type & Also Can Be Collected By The Player

---
+++ Assets/_Project/Scripts/Power Ups/PowerUpType.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SpaceDefender.PowerUps
+{
+    public class PowerUpType : MonoBehaviour
+    {
+        private enum powerUpType
+        {
+            TripleShot,
+            SpeedBoost,
+            ShieldBoost
+        }
+
+        [SerializeField] private powerUpType _powerUpType;
+
+
+
+        private void OnTriggerEnter2D(Collider2D collision)
+        {
+            if (collision.CompareTag("Player"))
+            {
+                switch (_powerUpType)
+                {
+                    case powerUpType.TripleShot:
+                        Debug.Log("Power Up Collected: " + gameObject.name);
+                        break;
+                    case powerUpType.SpeedBoost:
+                        Debug.Log("Power Up Collected: " + gameObject.name);
+                        break;
+                    case powerUpType.ShieldBoost:
+                        Debug.Log("Power Up Collected: " + gameObject.name);
+                        break;
+                    default:
+                        break;
+                }
+
+                Destroy(this.gameObject);
+            }
+        }
+    }
+}
+
