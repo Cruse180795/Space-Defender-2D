88e8bc8270604221703b9668204bf7c4f1965bbb
Ryan Cruse <54505658+Cruse180795@users.noreply.github.com>
2020-11-11 11:23:59 +0000

    Game Theme Added

---
+++ Assets/_Project/Scripts/Manager/AudioManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SpaceDefender.Manager
+{
+    public class AudioManager : MonoBehaviour
+    {
+        private void Awake()
+        {
+            SetUpSingleton();
+        }
+
+
+        private void SetUpSingleton()
+        {
+            if(FindObjectsOfType(GetType()).Length > 1)
+            {
+                Destroy(this.gameObject);
+            }
+            else
+            {
+                DontDestroyOnLoad(this.gameObject);
+            }
+        }
+    }
+}
+
