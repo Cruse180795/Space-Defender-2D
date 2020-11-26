4b77c720f214190f691e2b201c8e10f723685cad
Ryan Cruse <54505658+Cruse180795@users.noreply.github.com>
2020-11-26 16:45:25 +0000

    Phase 2 : Core Programming : PickUp Collect

--- Assets/_Project/Scripts/Power Ups/PowerUpMover.cs
+++ Assets/_Project/Scripts/Power Ups/PowerUpMover.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using SpaceDefender.Player;
 
 namespace SpaceDefender.PowerUps
 {
@@ -9,6 +10,25 @@ namespace SpaceDefender.PowerUps
         [SerializeField] private float _powerUpMoveSpeed = 5f;
 
 
+        private PowerUpType _powerUpType;
+        private SetPlayerPostition _playerPos;
+
+        private void Start()
+        {
+            _powerUpType = GetComponent<PowerUpType>();
+            _playerPos = FindObjectOfType<SetPlayerPostition>();
+
+            if(_powerUpType == null)
+            {
+                Debug.LogError("The PowerUpType Is NULL");
+            }
+
+            if(_playerPos == null)
+            {
+                Debug.LogError("The PlayerPos Is NULL");
+            }
+        }
+
         private void Update()
         {
             Movement();
@@ -16,7 +36,16 @@ namespace SpaceDefender.PowerUps
 
         private void Movement()
         {
-            transform.Translate(Vector3.down * _powerUpMoveSpeed * Time.deltaTime);
+            if(_powerUpType.CanCollect == false)
+            {
+                transform.Translate(Vector3.down * _powerUpMoveSpeed * Time.deltaTime);
+            }
+            else
+            {
+                transform.position = Vector2.MoveTowards(transform.position, _playerPos.transform.position, 5f * Time.deltaTime);
+            }
+
+            
 
             if(transform.position.y < -6f)
             {
--- Assets/_Project/Scripts/Power Ups/PowerUpType.cs
+++ Assets/_Project/Scripts/Power Ups/PowerUpType.cs
@@ -18,6 +18,37 @@ namespace SpaceDefender.PowerUps
         [SerializeField] private powerUpType _powerUpType;
         [SerializeField] private AudioClip _powerUpClip;
         [Range(0.1f, 1f)] [SerializeField] private float _clipVolume = 0.5f;
+        [SerializeField] private float _collectionRange = 1.5f;
+
+
+        private bool _inRange = false;
+        private bool _canCollect = false;
+
+        public bool CanCollect
+        {
+            get
+            {
+                return _canCollect;
+            }
+            set
+            {
+                _canCollect = value;
+            }
+        }
+
+
+        private void Update()
+        {
+            if(Input.GetKeyDown(KeyCode.C) && _inRange == true)
+            {
+                _canCollect = true;
+            }
+        }
+
+        private void FixedUpdate()
+        {
+            QuickCollectPowerUp();
+        }
 
 
         private void OnTriggerEnter2D(Collider2D collision)
@@ -64,6 +95,25 @@ namespace SpaceDefender.PowerUps
             AudioSource.PlayClipAtPoint(_powerUpClip, Camera.main.transform.position, _clipVolume);
             Destroy(this.gameObject);
         }
+
+
+        private void OnDrawGizmos()
+        {
+            Gizmos.color = Color.red;
+            Gizmos.DrawWireSphere(transform.position, _collectionRange);
+        }
+
+        private void QuickCollectPowerUp()
+        {
+            Collider2D collider = Physics2D.OverlapCircle(transform.position, _collectionRange);
+
+            if (collider.CompareTag("Player"))
+            {
+                Debug.Log("Player In Range");
+                _inRange = true;
+            }
+        }
+
     }
 }
 
